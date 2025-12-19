428b3efc86eb742a80b694940d828189745de105
erykovsky <jageryk19@gmail.com>
2025-12-19 12:24:20 +0100

    zaktualizowano plik konfiguracyjny next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- experimental: {
   reactCompiler: true,
-  ppr: true,
- },
- images: {
-  remotePatterns: [
-   {
-    protocol: "https",
-    hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
-    port: "",
-    pathname: "/**",
-    search: "",
-   },
-  ],
- },
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
