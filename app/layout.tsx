bb5981504fbe608fab83c8a5b7ef052d67cb5c9d
erykovsky <jageryk19@gmail.com>
2026-02-09 03:15:28 +0100

    poprawa wydajnosci strony

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: "device-width",
-  initialScale: 5,
+  initialScale: 1,
 };
 
 export default function RootLayout({
