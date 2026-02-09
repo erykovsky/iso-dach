3511ce2776de3962bd84f105964f03420b234d06
erykovsky <jageryk19@gmail.com>
2026-02-09 02:54:07 +0100

    d

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { PartnersSection } from "@/components/partners-section";
 export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
-            <main className="flex-grow">
+            <main className="grow">
                 <HeroCarousel />
                 <ServicesSection />
                 <BenefitsSection />
